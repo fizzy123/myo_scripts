9979dfb836a326eefaea394e82ab68d9d8e75313
Nobel Yoo <nobr@nobr.me>
2015-01-05 23:37:21 -0800

    Changes for direcational waves

--- test.lua
+++ test.lua
@@ -3,10 +3,31 @@ scriptTitle = "Output Everything"
 scriptDetailsUrl = "" -- We don't have this until it's submitted to the Myo Market
 
 function onPoseEdge(pose, edge)
-    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+  roll = myo.getRoll() * 180 / 3.14
+  myo.debug('MYo roll: ' .. roll)
+  if pose == "waveIn" then
+    if edge == "on" then
+	  if roll < 60 then
+	    myo.debug("down")
+	  elseif roll > 60 then
+	    myo.debug("left")
+	  end
+    end
+  end
+
+  if pose == "waveOut" then
+    if edge == "on" then
+      if roll < 60 then
+	    myo.debug("up")
+	  elseif roll > 60 then
+	    myo.debug("right")
+	  end
+    end
+  end
 end
 
 function onPeriodic()
+	roll = myo.getRoll() * 180 / 3.14
 end
 
 function onForegroundWindowChange(app, title)
